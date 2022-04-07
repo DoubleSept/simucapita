955bbe9cfe43491a21fbfbfb959e98a73c762bdd
DoubleSept <contact@doublesept.fr>
2022-04-07 22:51:54 +0200

    Avec changement de nombre

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,20 @@
-import { MDBBtn, MDBCol, MDBRow, MDBContainer, MDBInput } from 'mdb-react-ui-kit';
-import { useState } from 'react';
+import { MDBBtn, MDBCol, MDBRow, MDBContainer, MDBInput, MDBBadge, MDBCard, MDBCardBody, MDBCardTitle, MDBCardText } from 'mdb-react-ui-kit';
+import { useState, useEffect } from 'react';
 import './Body.scss'
 
 function Body() {
-    const nbParticipants = 1000
-    const nbParParticipant = 1000
+    const nbParticipants = 100
+    const nbParParticipant = 500
     var statutInitial = Array(nbParticipants).fill(nbParParticipant).map((element, index) => { return { numero: index, argent: element } })
 
     var [statutActuel, setStatut] = useState(statutInitial)
     var [dernierEchange, setDernierEchange] = useState([0, 0])
     var [champNbFois, setChampNbFois] = useState(10000)
 
+    var [etapeActuelle, setEtapeActuelle] = useState(1)
+
     const changerStatut = (nouveauStatut) => {
-        nouveauStatut.sort((a, b) => b.argent - a.argent)
+        nouveauStatut.sort((a, b) => a.argent - b.argent)
         setStatut(nouveauStatut)
     }
 
@@ -20,20 +22,27 @@ function Body() {
         var receveur = Math.floor(Math.random() * nbParticipants);
         var donneur = Math.floor(Math.random() * nbParticipants);
 
-        var statutSortie = Array.from(statutEntree);
-        statutSortie[donneur] = { ...statutEntree[donneur], argent: statutEntree[donneur].argent - 1 }
-        statutSortie[receveur] = { ...statutEntree[receveur], argent: statutEntree[receveur].argent + 1 }
+        if (statutEntree[donneur].argent > 0) {
+            statutEntree[donneur] = { ...statutEntree[donneur], argent: statutEntree[donneur].argent - 1 }
+            statutEntree[receveur] = { ...statutEntree[receveur], argent: statutEntree[receveur].argent + 1 }
+        }
 
-        return [statutSortie, donneur, receveur];
+        return [statutEntree, donneur, receveur];
     }
 
-    const jouerUneFois = (evenement) => {
+    const jouerUneFoisSimple = () => {
 
         var statutTemporaire = Array.from(statutActuel);
         var [nouveauStatut, donneur, receveur] = unCoup(statutTemporaire);
 
         changerStatut(nouveauStatut);
         setDernierEchange([donneur, receveur]);
+        setEtapeActuelle(+(etapeActuelle) + 1);
+    }
+
+    const jouerUneFois = (evenement) => {
+
+        jouerUneFoisSimple();
 
         evenement.stopPropagation();
         evenement.preventDefault();
@@ -46,6 +55,7 @@ function Body() {
             statutTemporaire = unCoup(statutTemporaire)[0];
         }
         changerStatut(statutTemporaire);
+        setEtapeActuelle(+(etapeActuelle) + nbFois);
 
         evenement.stopPropagation();
         evenement.preventDefault();
@@ -59,36 +69,72 @@ function Body() {
         <MDBContainer className='g-2'>
             <MDBRow className='g-2'>
                 <MDBCol md="6" className='gauche p-3'>
-                    <ol>
-                        {statutActuel.sort().map((element, index) => {
-                            return (
-                                <li key={element.numero}>
-                                    <b>{String(element.numero).padStart(4, '0')}</b> possède <b>{element.argent}$</b>
-                                </li>
-                            );
-                        })
-                        }
-                    </ol>
+                    <MDBContainer>
+                        <MDBRow>
+                            <MDBCol md="6">
+                                <MDBCard md="4">
+                                    <MDBCardBody>
+                                        <MDBCardTitle>LES RICHES</MDBCardTitle>
+                                        <MDBCardText>
+                                            <ol>
+                                                {statutActuel.map((element) => {
+                                                    return (
+                                                        <li key={element.numero}>
+                                                            <b>{String(element.numero).padStart(4, '0')}</b> possède <b>{element.argent}$</b>
+                                                        </li>
+                                                    );
+                                                })
+                                                }
+                                            </ol>
+                                        </MDBCardText>
+                                    </MDBCardBody>
+                                </MDBCard>
+                            </MDBCol>
+                            <MDBCol md="6">
+                                <MDBCard md="4">
+
+                                    <MDBCardBody>
+                                        <MDBCardTitle>LES PAUVRES</MDBCardTitle>
+                                        <MDBCardText>
+                                            <ol>
+                                                {statutActuel.reverse().map((element) => {
+                                                    return (
+                                                        <li key={element.numero}>
+                                                            <b>{String(element.numero).padStart(4, '0')}</b> possède <b>{element.argent}$</b>
+                                                        </li>
+                                                    );
+                                                })
+                                                }
+                                            </ol>
+                                        </MDBCardText>
+                                    </MDBCardBody>
+                                </MDBCard>
+                            </MDBCol>
+                        </MDBRow>
+                    </MDBContainer>
                 </MDBCol>
                 <MDBCol md="6" className='droite p-3'>
                     <MDBContainer>
                         <MDBRow className='g-2'>
-                            <MDBCol md="12">
-                                <MDBBtn color="primary" onClick={jouerUneFois}>Cliquer Ici</MDBBtn>
+                            <MDBCol md="12" center style={{ textAlign: 'center' }}>
+                                <h2>
+                                    Etape
+                                    <MDBBadge className='ms-2'>{etapeActuelle}</MDBBadge>
+                                </h2>
                             </MDBCol>
-                            <MDBCol md="12">
-                                <p>{dernierEchange[0]} => {dernierEchange[1]}</p>
-                            </MDBCol>
-                            <MDBCol md="12" >
-                                <MDBBtn color="warning" onClick={jouerMilleFois}>Cliquer ici pour jouer mille fois</MDBBtn>
-                            </MDBCol>
+                            <MDBBtn md="12" color="success" onClick={jouerUneFois}>Jouer une fois</MDBBtn>
+                            <MDBBtn md="12" color="warning" onClick={jouerMilleFois}>Jouer mille fois</MDBBtn>
                             <MDBCol md="6" >
-                                <MDBInput label='Nb fois' id='fois' type='number' value={champNbFois} onChange={(e) => setChampNbFois(e.target.value)} />
+                                <MDBInput
+                                    id='fois' type='number'
+                                    value={champNbFois}
+                                    onChange={(e) => setChampNbFois(e.target.value)}
+                                    style={{ backgroundColor: 'rgba(255,255,255,0.75)' }} />
                             </MDBCol>
                             <MDBCol md="6" >
                                 <MDBBtn color="secondary" onClick={(e) => {
-                                    jouerNFois(e, champNbFois)
-                                }}>Jouer</MDBBtn>
+                                    jouerNFois(e, parseInt(champNbFois))
+                                }} style={{ width: '100%' }}>Jouer</MDBBtn>
                             </MDBCol>
                         </MDBRow>
                     </MDBContainer>
